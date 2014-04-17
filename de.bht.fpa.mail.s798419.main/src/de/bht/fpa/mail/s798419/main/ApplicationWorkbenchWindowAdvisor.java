b3f15e9bcd475cdb7daf41d944068b2075dd9d66
Duve, Michael <michael.duve@bild.de>
2014-04-17 15:34:02 +0200

    check style and initial configuration

--- de.bht.fpa.mail.s798419.main/src/de/bht/fpa/mail/s798419/main/ApplicationWorkbenchWindowAdvisor.java
+++ de.bht.fpa.mail.s798419.main/src/de/bht/fpa/mail/s798419/main/ApplicationWorkbenchWindowAdvisor.java
@@ -8,19 +8,24 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        super(configurer);
-    }
+  static final int WINDOW_WIDTH = 800;
+  static final int WINDOW_HEIGHT = 600;
 
-    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
-        return new ApplicationActionBarAdvisor(configurer);
-    }
-    
-    public void preWindowOpen() {
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(400, 300));
-        configurer.setShowCoolBar(false);
-        configurer.setShowStatusLine(false);
-        configurer.setTitle("Hello RCP"); //$NON-NLS-1$
-    }
+  public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+    super(configurer);
+  }
+
+  @Override
+  public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+    return new ApplicationActionBarAdvisor(configurer);
+  }
+
+  @Override
+  public void preWindowOpen() {
+    IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+    configurer.setInitialSize(new Point(WINDOW_WIDTH, WINDOW_HEIGHT));
+    configurer.setShowCoolBar(false);
+    configurer.setShowStatusLine(false);
+    configurer.setTitle("FPA Mailer"); //$NON-NLS-1$
+  }
 }
